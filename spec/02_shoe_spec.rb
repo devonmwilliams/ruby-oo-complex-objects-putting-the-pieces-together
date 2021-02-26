af10989018ce47c84dd22e686dd18f8ee1066087
Devon Williams <devonmaddenwilliams@gmail.com>
2021-02-25 22:31:40 -0500

    commit tree 14b374c736f950157c5fe84808bc5230b9757de1

--- spec/02_shoe_spec.rb
+++ spec/02_shoe_spec.rb
@@ -6,7 +6,7 @@ describe 'Shoe' do
       expect{Shoe.new("Adidas")}.to_not raise_error
     end
   end
-
+  
   describe 'properties' do
     it 'has a brand' do
       # When must the brand be assigned for this to work?
@@ -45,5 +45,70 @@ describe 'Shoe' do
       shoe.cobble
       expect(shoe.condition).to eq("new")
     end
+  end
+end
+
+class Shoe
+  def initialize(brand)
+        @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+
+  def color=(color)
+    @color = color
+  end
+
+  def color
+    @color
+  end
+
+  def size=(num)
+    @size = num
+  end
+
+  def size
+    @size
+  end
+
+  def material=(material)
+    @material = material
+  end
+
+  def material
+    @material
+  end
+
+  def condition=(condition)
+    @condition = condition
+  end
+
+  def condition
+    @condition
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
+  end
+end
+
+class Shoe
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+
+  def initialize(brand)
+        @brand = brand
+  end
+  
+  def brand
+    @brand
+  end
+
+  def cobble
+    @condition = "new"
+    puts "Your shoe is as good as new!"
   end
 end
